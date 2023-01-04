07d6984894d9d6c10f1f548fbbd92203586d7bc2
wil <nevskiy.vilyam@wb.ru>
2023-01-05 00:02:13 +0400

    base structure refactor

--- app/internal/config/config.go
+++ app/internal/config/config.go
@@ -1,1 +1,18 @@
-package internal
+package config
+
+type Config struct {
+	IsDebug       bool `env:"IS_DEBUG" env-default:"false"`
+	IsDevelopment bool `env:"IS_DEv" env-default:"false"`
+	Listen        struct {
+		Type   string `env:"LISTEN_TYPE" env-default:"port"` // приложение находится на порту
+		BindIP string `env:"BIND_IP" env-default:"0.0.0.0"`  // приложение на unix сокете
+		Port   string `env:"PORT" env-default:"10000"`       // по умолчанию приложение находится на 10000 порту
+	}
+	AppConfig struct {
+		LogLeveL  string
+		AdminUser struct {
+			Email    string `env:"ADMIN_EMAIL" env-required:"true"`
+			Password string `env:"ADMIN_PWD" env-required:"true"`
+		}
+	}
+}
